346220ae58655ed8365f97201d637434f28d88a7
Antony Male <antony.male@gmail.com>
2014-02-25 20:45:51 +0000

    Start work on MasterDetail sample

---
+++ Samples/Stylet.Samples.MasterDetail/ShellViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stylet.Samples.MasterDetail
+{
+    public class ShellViewModel : Screen
+    {
+        public IObservableCollection<EmployeeModel> Employees { get; private set; }
+
+        private EmployeeModel _selectedEmployee;
+        public EmployeeModel SelectedEmployee
+        {
+            get { return this._selectedEmployee; }
+            set
+            {
+                this._selectedEmployee = value;
+                this.NotifyOfPropertyChange();
+            }
+        }
+
+        public ShellViewModel()
+        {
+            this.DisplayName = "Master-Detail";
+
+            this.Employees = new BindableCollection<EmployeeModel>();
+
+            this.Employees.Add(new EmployeeModel() { Name = "Fred" });
+        }
+
+        public void RemoveItem(EmployeeModel item)
+        {
+            this.Employees.Remove(item);
+        }
+    }
+}
