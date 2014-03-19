5c8d3b518afc66cf37e2ca6ede45e9225f1c6813
Antony Male <antony.male@gmail.com>
2014-03-19 20:34:26 +0000

    Add example with a custom ViewManager

---
+++ Samples/Stylet.Samples.OverridingViewManager/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stylet.Samples.OverridingViewManager
+{
+    public class Bootstrapper : Bootstrapper<ShellViewModel>
+    {
+        protected override void ConfigureIoC(StyletIoC.IStyletIoCBuilder builder)
+        {
+            base.ConfigureIoC(builder);
+
+            builder.Bind<IViewManager>().To<CustomViewManager>();
+        }
+    }
+}
