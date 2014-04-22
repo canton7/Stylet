43271b18fdce328eb053af766a13d464b114e8d9
Antony Male <antony.male@gmail.com>
2014-04-22 12:53:53 +0100

    Remove debugging from install.ps1

--- NuGet/tools/install.ps1
+++ NuGet/tools/install.ps1
@@ -1,7 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
 
-Write-Host "Hello"
-Write-Host $project.FullName
 $path = [System.IO.Path]
 $readmefile = $path::Combine($path::GetDirectoryName($project.FullName), "StyletReadme.txt")
 $DTE.ItemOperations.OpenFile($readmefile)
