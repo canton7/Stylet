a0f8590f9cba7bdde5d9b661a7b836e124da99e9
Antony Male <antony.male@gmail.com>
2014-05-01 17:45:34 +0100

    Add LambdaComparer

---
+++ Stylet/LambdaComparer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stylet
+{
+    /// <summary>
+    /// IEqualityComparer{T} implementation which uses a Lambda
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LambdaComparer<T> : IEqualityComparer<T>
+    {
+        Func<T, T, bool> comparer;
+
+        /// <summary>
+        /// Create a new LambdaComparer{T}
+        /// </summary>
+        /// <param name="comparer">Comparer, which takes two {T} instances and returns true if they are equal</param>
+        public LambdaComparer(Func<T, T, bool> comparer)
+        {
+            this.comparer = comparer;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return this.comparer(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
