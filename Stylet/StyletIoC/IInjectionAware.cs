6533b34757704e66e88e06538ab059179093d87d
Antony Male <antony.male@gmail.com>
2014-07-21 10:57:18 +0100

    Split out the type which were all previously in StyletIoCContainer.cs

---
+++ Stylet/StyletIoC/IInjectionAware.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StyletIoC
+{
+    /// <summary>
+    /// Interface to be implemented by objects if they want to be notified when property injection has occurred
+    /// </summary>
+    public interface IInjectionAware
+    {
+        /// <summary>
+        /// Called by StyletIoC when property injection has occurred
+        /// </summary>
+        void ParametersInjected();
+    }
+}
---
+++ Stylet/StyletIoC/TypeExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace StyletIoC
+{
+    internal static class TypeExtensions
+    {
+        public static IEnumerable<Type> GetBaseTypesAndInterfaces(this Type type)
+        {
+            return type.GetInterfaces().Concat(type.GetBaseTypes());
+        }
+
+        public static IEnumerable<Type> GetBaseTypes(this Type type)
+        {
+            for (var baseType = type.BaseType; baseType != null; baseType = baseType.BaseType)
+            {
+                yield return baseType;
+            }
+        }
+
+        public static bool Implements(this Type implementationType, Type serviceType)
+        {
+            return serviceType.IsAssignableFrom(implementationType) ||
+                implementationType.GetBaseTypesAndInterfaces().Any(x => x == serviceType || (x.IsGenericType && x.GetGenericTypeDefinition() == serviceType));
+        }
+
+        private static readonly Dictionary<Type, string> primitiveNameMapping = new Dictionary<Type, string>()
+        {
+            { typeof(byte), "byte" },
+            { typeof(sbyte), "sbyte" },
+            { typeof(char), "char" },
+            { typeof(short), "short" },
+            { typeof(ushort), "ushort" },
+            { typeof(int), "int" },
+            { typeof(uint), "uint" },
+            { typeof(long), "long" },
+            { typeof(ulong), "ulong" },
+            { typeof(float), "float" },
+            { typeof(double), "double" },
+            { typeof(decimal), "decimal" },
+            { typeof(bool), "bool" },
+        };
+
+        public static string Description(this Type type)
+        {
+            if (type.IsGenericTypeDefinition)
+                return String.Format("{0}<{1}>", type.Name.Split('`')[0], String.Join(", ", type.GetTypeInfo().GenericTypeParameters.Select(x => x.Name)));
+            var genericArguments = type.GetGenericArguments();
+            if (genericArguments.Length > 0)
+            {
+                return String.Format("{0}<{1}>", type.Name.Split('`')[0], String.Join(", ", genericArguments.Select(x =>
+                {
+                    string name;
+                    return primitiveNameMapping.TryGetValue(x, out name) ? name : x.Name;
+                })));
+            }
+            return type.Name;
+        }
+    }
+}
