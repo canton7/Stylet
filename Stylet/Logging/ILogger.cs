e2dc463d01377c92f90fdfbec8cf3b6d9abd3e95
Antony Male <antony.male@gmail.com>
2015-01-18 22:15:05 +0000

    Sort logging classes into different files

---
+++ Stylet/Logging/ILogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Stylet.Logging
+{
+    /// <summary>
+    /// Logger used by Stylet for internal logging
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Log the message as info
+        /// </summary>
+        /// <param name="format">A formatted message</param>
+        /// <param name="args">format parameters</param>
+        void Info(string format, params object[] args);
+
+        /// <summary>
+        /// Log the message as a warning
+        /// </summary>
+        /// <param name="format">A formatted message</param>
+        /// <param name="args">format parameters</param>
+        void Warn(string format, params object[] args);
+
+        /// <summary>
+        /// Log an exception as an error
+        /// </summary>
+        /// <param name="exception">Exception to log</param>
+        /// <param name="message">Additional message to add to the exception</param>
+        void Error(Exception exception, string message = null);
+    }
+}
---
+++ Stylet/Logging/NullLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Stylet.Logging
+{
+    /// <summary>
+    /// ILogger implementation which does nothing - used by default
+    /// </summary>
+    public class NullLogger : ILogger
+    {
+        /// <summary>
+        /// Log the message as info
+        /// </summary>
+        /// <param name="format">A formatted message</param>
+        /// <param name="args">format parameters</param>
+        public void Info(string format, params object[] args) { }
+
+        /// <summary>
+        /// Log the message as a warning
+        /// </summary>
+        /// <param name="format">A formatted message</param>
+        /// <param name="args">format parameters</param>
+        public void Warn(string format, params object[] args) { }
+
+        /// <summary>
+        /// Log an exception as an error
+        /// </summary>
+        /// <param name="exception">Exception to log</param>
+        /// <param name="message">Additional message to add to the exception</param>
+        public void Error(Exception exception, string message = null) { }
+    }
+}
